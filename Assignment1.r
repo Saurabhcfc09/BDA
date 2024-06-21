7e980b0d8efbe1737908d3f5a2bc6cfc934c9d01
Saurabhcfc09 <157640196+Saurabhcfc09@users.noreply.github.com>
2024-06-21 12:01:49 -0400

    Add files via upload

---
+++ Assignment1.r
@@ -0,0 +1,30 @@
+# Define the vector of numbers
+n <- c(7, 12, 5)
+avg <- mean(n)
+print(avg) 
+
+# Define your first name
+first_name <- "Manogna"
+cat("Welcome to R,", first_name)
+
+# Define the variables
+x <- 7
+y <- 12
+z <- 4
+if (x > z) {
+  cat("Is x bigger than z? TRUE\n")
+} else {
+  cat("Is x bigger than z? FALSE\n")
+}
+if (x > y) {
+  cat("Is x bigger than y? TRUE\n")
+} else {
+  cat("Is x bigger than y? FALSE\n")
+}
+
+
+
+
+
+
+
